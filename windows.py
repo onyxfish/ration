1be600e37341e7e9daf58c65d4d91827fe93536f
Christopher Groskopf <staringmonkey@gmail.com>
2010-07-16 22:47:33 -0500

    Added status icon.

---
+++ windows.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import re
+import subprocess
+
+def get_screen_resolution():
+    p = subprocess.Popen(['xwininfo', '-root'], stdout=subprocess.PIPE)
+    output = p.communicate()[0]
+    
+    match = re.search(r'Width: ([0-9]+)\s+Height: ([0-9]+)', output)
+    
+    width = int(match.group(1))
+    height = int(match.group(2))
+    
+    return width, height
