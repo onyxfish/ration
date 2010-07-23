592e80225640e707440615c861235f5e391c8e7a
Christopher Groskopf <staringmonkey@gmail.com>
2010-07-22 21:52:57 -0500

    More deployment voodoo.

--- setup.py
+++ setup.py
@@ -9,6 +9,21 @@ setup(name='ration',
       author_email='staringmonkey@gmail.com',
       url='http://github.com/bouvard/ration',
       license='MIT',
+      
+      classifiers=[
+                     'Development Status :: 3 - Alpha',
+                     'Environment :: X11 Applications :: Gnome',
+                     'Intended Audience :: End Users/Desktop',
+                     'License :: OSI Approved :: MIT License',
+                     'Natural Language :: English',
+                     'Operating System :: POSIX :: Linux',
+                     'Programming Language :: Python :: 2.6',
+                     'Topic :: Desktop Environment :: Gnome',
+                     'Topic :: Utilities'
+                    ],
+      
+      long_description='A Divvy-like application for quickly arranging and resizing windows in GTK-based Linux distributions.',
+      
       packages=['ration'],
       scripts=['scripts/ration'],
       data_files=[('share/applications', ['data/ration.desktop'])]
